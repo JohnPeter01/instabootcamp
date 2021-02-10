e723f58321734d7bb6f581a8b44df103a29018a8
João Pedro <joao.pedro.fehc@gmail.com>
2021-02-10 19:53:07 -0300

    Adicionando a base do menu.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import styled from 'styled-components'
+import Menu from './src/components/commons/Menu'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-function Title2(porps){
-  return <h1>TESTANDO {porps.children}</h1>
-}
 export default function Home() {
-  return <>
-  <Title2>TITULO</Title2>
-    <Title>TESTANDO BUILD...</Title>
-  </>
+  return (
+    <Menu/>
+  )
 }
---
+++ pages/src/components/commons/Menu/styles/MenuWrapper.js
@@ -0,0 +1,56 @@
+
+import styled from "styled-components";
+/*
+    Essa é tanto uma prefencia quanto uma boa pratica, separar as tags de estilos em um arquivo separado.
+    Em uma pasta styles criar um arquivo com o nome da tag criada.
+*/
+export const MenuWrapper =  styled.nav`
+  font-family: 'Rubik', sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 18px;
+  padding-left: 28px;
+  padding-right:  28px;
+`
+
+MenuWrapper.LeftSide = styled.div`
+  padding:0;
+  margin:0;
+  order:1;
+`;
+MenuWrapper.CentralSide = styled.div`
+    padding:0;
+    margin:0;
+    order:3;
+    width:100%;
+    list-style:none;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    margin-top: 17px;
+    border-top: 1px solid #88989E;
+    border-bottom: 1px solid #88989E;
+    padding:12px;
+    a{
+      text-align: center;
+      display: block;
+      text-decoration: none;
+      color: #88989E;
+      transition: 200ms ease-in-out;
+      &:hover,
+      &:focus{
+        font-weight: 500;
+        color: #070C0E;
+      }
+    }
+`;
+MenuWrapper.RightSide = styled.div`
+    padding:0;
+    margin:0;
+    order:2;
+    display:flex;
+    flex: 1;
+    justify-content: flex-end;
+`;
