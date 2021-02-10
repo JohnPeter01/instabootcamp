39e194f3cdbcda88a9fe7a79803fa2a17c7b9c15
João Pedro <joao.pedro.fehc@gmail.com>
2021-02-10 18:19:42 -0300

    Commit Inicial

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
-
+function Title2(porps){
+  return <h1>TESTANDO {porps.children}</h1>
+}
 export default function Home() {
-  return <Title>My page</Title>
+  return <>
+  <Title2>TITULO</Title2>
+    <Title>TESTANDO BUILD...</Title>
+  </>
 }
