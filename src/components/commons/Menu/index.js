cb18b29d92051364b34c4e341356482233938cfd
João Pedro <joao.pedro.fehc@gmail.com>
2021-02-19 20:44:06 -0300

    Finalização Modulo 1

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,6 +1,7 @@
+
 import React from 'react';
 import { Logo } from '../../../theme/Logo';
-import  { Text }  from '../../foundation/Text';
+import { Text } from '../../foundation/Text';
 import { Button } from '../Button';
 import { MenuWrapper } from './styles/MenuWrapper';
 
@@ -8,34 +9,21 @@ import { MenuWrapper } from './styles/MenuWrapper';
   Padrão criar uma pasta com o nome do componente e dentro dela fica o index.js com a implementação.
 */
 
-export default function Menu(){
-    const links = [
-        {
-          nome: 'Home',
-            url:'/'
-        },
-        {
-          nome: 'Perguntas Frequentes',
-            url:'/faq'
-        },
-        {
-            nome: 'Sobre',
-            url:'/sobre'
-        }
-
-    ]
-  return(
+export default function Menu() {
+  return (
     <MenuWrapper>
       <MenuWrapper.LeftSide> {/* MenuWrapper.LeftSide */}
         <Logo />
       </MenuWrapper.LeftSide>
       <MenuWrapper.CentralSide as="ul"> {/* MenuWrapper.CentralSide */}
-        {links.map((link) => (
-          //Sempre que tiver uma Li é necessario ter um indetificador unico para um elemento
-          //, mesmo que não haja diferença de comportamento.
-          <li key={link.url}> 
+        {[
+          { url: '/', name: 'Home' },
+          { url: '/faq', name: 'Perguntas Frequentes' },
+          { url: '/sobre', name: 'Sobre' },
+        ].map((link) => (
+          <li key={link.url}>
             <Text variant="smallestException" tag="a" href={link.url}>
-              {link.nome}
+              {link.name}
             </Text>
           </li>
         ))}
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styled, { css } from 'styled-components';
+import get from 'lodash/get';
 import PropTypes from 'prop-types';
-import get from 'lodash/get'
-import styled, { css } from 'styled-components'
-import { propToStyle } from '../../../theme/Utils/propTostyle';
-import { breakpointsMedia } from '../../../theme/Utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const paragraph1 = css`
   ${({ theme }) => css`
@@ -22,51 +22,57 @@ const smallestException = css`
 `;
 
 export const TextStyleVariants = {
-    smallestException,
-    paragraph1,
-    title: css`
-      ${({ theme }) => css`
-        font-size: ${theme.typographyVariants.titleXS.fontSize};
-        font-weight: ${theme.typographyVariants.titleXS.fontWeight};
-        line-height: ${theme.typographyVariants.titleXS.lineHeight};
-      `}
-      ${breakpointsMedia({
-        md: css`
-          ${({ theme }) => css`
-            font-size: ${theme.typographyVariants.title.fontSize};
-            font-weight: ${theme.typographyVariants.title.fontWeight};
-            line-height: ${theme.typographyVariants.title.lineHeight};
-          `}
-        `,
-      })}
-    `,
-  };
+  smallestException,
+  paragraph1,
+  title: css`
+    ${({ theme }) => css`
+      font-size: ${theme.typographyVariants.titleXS.fontSize};
+      font-weight: ${theme.typographyVariants.titleXS.fontWeight};
+      line-height: ${theme.typographyVariants.titleXS.lineHeight};
+    `}
+    ${breakpointsMedia({
+      md: css`
+        ${({ theme }) => css`
+          font-size: ${theme.typographyVariants.title.fontSize};
+          font-weight: ${theme.typographyVariants.title.fontWeight};
+          line-height: ${theme.typographyVariants.title.lineHeight};
+        `}
+      `,
+    })}
+  `,
+};
 
-  const TextBase = styled.span`
+const TextBase = styled.span`
   ${({ variant }) => TextStyleVariants[variant]}
   color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
   ${propToStyle('textAlign')}
 `;
 
-export function Text({tag, variant, children, ...props}){
-    return(
-        <TextBase
-            as={tag}
-            variant = {variant}
-            {...props}
-        >
-            {children}
-        </TextBase>
-    );
+export function Text({
+  variant,
+  children,
+  tag,
+  ...props
+}) {
+  return (
+    <TextBase
+      as={tag}
+      variant={variant}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {children}
+    </TextBase>
+  );
 }
 
 Text.defaultProps = {
-    tag: 'span',
-    variant:'paragraph1'
-}
+  tag: 'span',
+  variant: 'paragraph1',
+};
 
 Text.propTypes = {
-    children: PropTypes.node.isRequired,
-    tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
-    variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
-  };
+  children: PropTypes.node.isRequired,
+  tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
+  variant: PropTypes.oneOf(['title', 'paragraph1', 'smallestException']),
+};
