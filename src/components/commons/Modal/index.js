4140503413b88454261f7b79a0a7890ccd8f9a5b
João Pedro <joao.pedro.fehc@gmail.com>
2021-02-25 18:42:23 -0300

    Criação da animação do modal.

--- src/components/commons/Modal/index.js
+++ src/components/commons/Modal/index.js
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion'
 
 const ModalWrapper = styled.div`
   display: flex;
@@ -13,6 +14,8 @@ const ModalWrapper = styled.div`
   bottom: 0;
   margin: auto;
   overflow: scroll;
+  transition: .3s;
+  z-index: 100;
   ${({ isOpen }) => {
     if (isOpen) {
       return css`
@@ -38,9 +41,27 @@ function Modal({isOpen, onClose,children}){
         }
       }}
     >
-      {children({
-        'data-modal-safe-area': 'true',
-      })}
+      <motion.div
+      variants={{
+        open:{
+          x:0,
+        },
+        closed:{
+          x:'-100%',
+        }
+      }}
+      animate = {isOpen ? 'open' : 'closed'}
+      transition ={{  
+        duration: 0.5,
+      }}
+      style={{
+        display: 'flex',
+        flex:1,
+      }}>
+        {children({
+          'data-modal-safe-area': 'true',
+        })}
+      </motion.div>
     </ModalWrapper>
   )
 }
