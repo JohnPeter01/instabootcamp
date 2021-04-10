432af3bc41fa3d11edb8ad8a3f7e158ef60c12e0
João Pedro <joao.pedro.fehc@gmail.com>
2021-04-10 12:17:17 -0300

    refactor: refatorando e criando modelo de trabalho com paginas estaticas

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import FAQScreen from '../src/components/screens/FAQScreen';
-import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
+import FAQScreen from '../../src/components/screens/FAQScreen';
+import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
 function FAQPage({ faqCategories }) {
   return (
