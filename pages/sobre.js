33e53e415ddd44ad3061fdd38d4b8d3f5b3b0d5f
João Pedro <joao.pedro.fehc@gmail.com>
2021-04-29 19:31:25 -0300

    feat: recuperando conteudo do DatoCMS

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+// import React from 'react';
+import AboutScreen from '../src/components/screens/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>
-        Página Sobre
-      </h1>
-    </div>
-  );
+export async function getStaticProps() {
+  const TOKEN = 'beefb99b05684611a2615b0c0c2a36';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+  query{
+    pageSobre{
+      pageTitle
+      pageDescripition
+    }
+  }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen);
