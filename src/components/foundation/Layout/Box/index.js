a9db0bbe755f926d01e452882bc55bad6c3c8445
João Pedro <joao.pedro.fehc@gmail.com>
2021-07-17 17:48:41 -0300

    fix(heart): corrigindo o posicionamento do coração de like

--- src/components/foundation/Layout/Box/index.js
+++ src/components/foundation/Layout/Box/index.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 import { propToStyle } from '../../../../theme/Utils/propToStyle';
 
 export const Box = styled.div`
-  ${propToStyle('flex')}
   ${propToStyle('display')}
   ${propToStyle('flexDirection')}
   ${propToStyle('justifyContent')}
+  ${propToStyle('alignItems')}
+  ${propToStyle('flex')}
   ${propToStyle('flexWrap')}
   ${propToStyle('backgroundImage')}
   ${propToStyle('backgroundColor')}
@@ -13,8 +14,20 @@ export const Box = styled.div`
   ${propToStyle('backgroundPosition')}
   ${propToStyle('boxShadow')}
   ${propToStyle('padding')}
-  ${propToStyle('marginTop')}
-  ${propToStyle('marginBottom')}
   ${propToStyle('width')}
   ${propToStyle('height')}
+  ${propToStyle('listStyle')}
+  ${propToStyle('margin')}
+  ${propToStyle('marginLeft')}
+  ${propToStyle('marginTop')}
+  ${propToStyle('marginBottom')}
+  ${propToStyle('marginRight')}
+  ${propToStyle('maxWidth')}
+  ${propToStyle('maxHeight')}
+  ${propToStyle('position')}
+  ${propToStyle('top')}
+  ${propToStyle('left')}
+  ${propToStyle('right')}
+  ${propToStyle('bottom')}
+  ${({ theme, borderRadiusTheme }) => borderRadiusTheme && `border-radius: ${theme.borderRadius}`};
 `;
